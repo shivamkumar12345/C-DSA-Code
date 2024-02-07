b93ad00a86b8a7d3a475f827cb2a5185dfe4de5d
shivam Kumar <sk9220449@gmail.com>
2024-02-07 17:00:42 +0530

    adding lecture 36

---
+++ lecture36/heightOfTree.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class TreeNode{
+     public:
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+   
+    TreeNode(int data){
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+TreeNode* constructATree(){
+    int x; cin>>x;
+    if(x == -1){
+        return NULL;
+    }
+    TreeNode * root = new TreeNode(x);
+
+    root->left = constructATree();
+    root->right = constructATree();
+    return root;
+}
+int height(TreeNode * root){
+    if(root == NULL)return 0;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    return max(lh,rh) +1;
+}
+int diameter(TreeNode * root){
+    if(root == NULL)return 0;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    int curr_dia = lh + rh;
+    int ld =diameter(root->left);
+    int rd = diameter(root->right);
+    return max(max(ld,rd),curr_dia);
+
+}
+int optimiseDiameter(TreeNode * root, int &diameter){
+    if(root==NULL){
+        return 0;
+    }
+    int left = optimiseDiameter(root->left, diameter);
+    int right = optimiseDiameter(root->right, diameter);
+    diameter = max(diameter, left + right);
+    return max(left , right) +1;
+}
+int main(){
+    TreeNode* root = constructATree();
+    cout<<height(root);
+    int diameter =0;
+    optimiseDiameter(root, diameter);
+    cout<<diameter;
+}
